1f6f06ec65fdf95c2d86de91591b754437d029f4
ChangSubKwak <paradise1732@gmail.com>
2021-10-10 03:22:17 +0900

    feat: 게임횟수를 의미하는 클래스(GameTryCount)의 생성 및 음수와 0값 입력시 예외 발생 기능 구현

---
+++ src/main/java/domain/GameTryCount.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class GameTryCount extends PositiveNumber {
+    private GameTryCount(int gameTryCount) {
+        super(gameTryCount);
+    }
+
+    public static GameTryCount from(int gameTryCount) {
+        return new GameTryCount(gameTryCount);
+    }
+
+    protected boolean enableZero() {
+        return false;
+    }
+}
