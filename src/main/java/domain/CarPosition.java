378a6b465e91ce054e67c68b9d5f511e9c664f30
ChangSubKwak <paradise1732@gmail.com>
2021-10-10 03:20:03 +0900

    feat: 자동차위치를 의미하는 클래스(CarPosition)의 생성 및 음수 입력시 예외 발생 기능 구현

---
+++ src/main/java/domain/CarPosition.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class CarPosition extends PositiveNumber {
+    private CarPosition(int carPosition) {
+        super(carPosition);
+    }
+
+    public static CarPosition from(int carPosition) {
+        return new CarPosition(carPosition);
+    }
+
+    protected boolean enableZero() {
+        return true;
+    }
+}
