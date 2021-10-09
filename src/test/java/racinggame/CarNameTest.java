5bf3e172a41fec2e0999e81e04c00849f49e7490
ChangSubKwak <paradise1732@gmail.com>
2021-10-10 02:23:50 +0900

    fix: 정적 팩터리메소드 호출 주체를 객체에서 클래스명으로 변경

--- src/test/java/racinggame/CarNameTest.java
+++ src/test/java/racinggame/CarNameTest.java
@@ -8,24 +8,22 @@ import org.junit.jupiter.api.Test;
 import domain.CarName;
 
 class CarNameTest {
-    CarName carName;
-
     @DisplayName("정적팩터리 메서드를 이용하여 객체를 생성하면 객체가 생성된다.")
     @Test
     void createTest() {
-        assertThat(carName.from("chang")).isInstanceOf(CarName.class);
+        assertThat(CarName.from("chang")).isInstanceOf(CarName.class);
     }
 
     @DisplayName("자동차의 이름이 5자 초과하면 예외가 발생한다.")
     @Test
     void exceptionTest1() {
-        assertThatThrownBy(() -> carName.from("changsubkwak")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> CarName.from("changsubkwak")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("자동차의 이름이 null 또는 빈문자이면 예외가 발생한다.")
     @Test
     void exceptionTest2() {
-        assertThatThrownBy(() -> carName.from(null)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> carName.from("")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> CarName.from(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> CarName.from("")).isInstanceOf(IllegalArgumentException.class);
     }
 }
