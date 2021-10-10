27bd2980f952853461562cb0cc7a1d8e1f5d112b
ChangSubKwak <paradise1732@gmail.com>
2021-10-11 03:28:12 +0900

    test: 무조건 전진 전략 클래스(AlwaysMovableStrategy) 생성 및 이동여부 테스트 케이스 구현

---
+++ src/test/java/racinggame/AlwaysMovableStrategyTest.java
@@ -0,0 +1,24 @@
+package racinggame;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import strategy.AlwaysMovableStrategy;
+import strategy.MovableStrategy;
+
+class AlwaysMovableStrategyTest {
+    @DisplayName("정적팩터리 메서드를 이용하여 객체를 생성하면 객체가 생성된다.")
+    @Test
+    void createTest() {
+        assertThat(AlwaysMovableStrategy.newInstance()).isInstanceOf(AlwaysMovableStrategy.class);
+    }
+
+    @DisplayName("항상 움직일 수 있는 전략울 선택하면 움직임 여부는 항상 참이다.")
+    @Test
+    void isMovableTest() {
+        MovableStrategy movableStrategy = AlwaysMovableStrategy.newInstance();
+        assertThat(movableStrategy.isMovable()).isTrue();
+    }
+}
