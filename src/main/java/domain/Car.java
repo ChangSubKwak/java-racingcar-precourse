12421b7a9e3da10fb550ae39adcf99cd94df2f21
ChangSubKwak <paradise1732@gmail.com>
2021-10-10 23:59:00 +0900

    test: 자동차의 이름과 위치를 가진 클래스(GameTryCount)의 생성 및 이름 예외 발생, 전진 테스트 케이스 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,34 @@
+package domain;
+
+public class Car {
+    public static final int INITIAL_POSITION = 0;
+
+    private final CarName carName;
+    private CarPosition carPosition;
+
+    private Car(String carName) {
+        this.carName = CarName.from(carName);
+        this.carPosition = CarPosition.from(INITIAL_POSITION);
+    }
+
+    private Car(String carName, int carPosition) {
+        this(carName);
+        this.carPosition = CarPosition.from(carPosition);
+    }
+
+    public static Car from(String carName) {
+        return new Car(carName);
+    }
+
+    public static Car of(String carName, int carPosition) {
+        return new Car(carName, carPosition);
+    }
+
+    public void forward(/* MovableStrategy movableStrategy */) {
+        carPosition = CarPosition.from(carPosition.positiveNumber() + 1);
+    }
+
+    public CarPosition position() {
+        return carPosition;
+    }
+}
