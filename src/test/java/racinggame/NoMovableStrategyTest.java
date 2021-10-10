fdb853094f462b78fef1a124d42a2d7b55d5e57f
ChangSubKwak <paradise1732@gmail.com>
2021-10-11 03:29:56 +0900

    test: 무조건 정지 전략 클래스(NoMovableStrategyT) 생성 및 이동여부 테스트 케이스 구현

---
+++ src/test/java/racinggame/NoMovableStrategyTest.java
@@ -0,0 +1,26 @@
+package racinggame;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import strategy.AlwaysMovableStrategy;
+import strategy.EveryNTimesMovableStrategy;
+import strategy.MovableStrategy;
+import strategy.NoMovableStrategy;
+
+class NoMovableStrategyTest {
+    @DisplayName("정적팩터리 메서드를 이용하여 객체를 생성하면 객체가 생성된다.")
+    @Test
+    void createTest() {
+        assertThat(NoMovableStrategy.newInstance()).isInstanceOf(NoMovableStrategy.class);
+    }
+
+    @DisplayName("항상 정지하는 전략울 선택하면 움직임 여부는 항상 거짓이다.")
+    @Test
+    void isMovableTest() {
+        MovableStrategy movableStrategy = NoMovableStrategy.newInstance();
+        assertThat(movableStrategy.isMovable()).isFalse();
+    }
+}
