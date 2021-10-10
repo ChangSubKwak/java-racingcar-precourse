580866a14a4afcf2527e701b8e199a1a14218bde
ChangSubKwak <paradise1732@gmail.com>
2021-10-11 03:38:05 +0900

    feat: PositiveNumber클래스에 객체 단위 비교를 위한 equals, hashCode 메서드 추가

--- src/main/java/domain/PositiveNumber.java
+++ src/main/java/domain/PositiveNumber.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public abstract class PositiveNumber {
     private static final String POSITIVE_NUMBER_MUST_NOT_BE_NEGATIVE_EXCEPTION_STATEMENT = "음수값은 올바르지 않습니다.";
     private static final String MUST_NOT_BE_ZERO_EXCEPTION_STATEMENT = "값이 0이 되면 안됩니다.";
@@ -22,9 +24,28 @@ public abstract class PositiveNumber {
         }
     }
 
+    protected int positiveNumber() {
+        return positiveNumber;
+    }
+
     private boolean isZero(int positiveNumber) {
         return !enableZero() && positiveNumber == ZERO_VALUE;
     }
 
     protected abstract boolean enableZero();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof PositiveNumber))
+            return false;
+        PositiveNumber that = (PositiveNumber)o;
+        return positiveNumber == that.positiveNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(positiveNumber);
+    }
 }
