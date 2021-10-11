87d302268c10235c147f09b019a1ddbf33648bd1
ChangSubKwak <paradise1732@gmail.com>
2021-10-12 01:45:05 +0900

    feat: 자동차 경주 게임 진행을 위한 컨트롤러 메서드 호출 추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,10 @@
 package racinggame;
 
+import controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
+        GameController gameController = GameController.newInstance();
+        gameController.outputResult(gameController.inputCars(), gameController.inputGameTryCount());
     }
 }
