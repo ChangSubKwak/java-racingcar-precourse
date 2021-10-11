d6afa7d4ba151750217c637f5147b3365fca6b6e
ChangSubKwak <paradise1732@gmail.com>
2021-10-11 16:29:49 +0900

    fix: (기능변경) N번마다 이동 전략 클래스(EveryNTimesMovableStrategy)를 2번마다 이동 전략 클래스(EveryTwiceMovableStrategy)으로 기능 변경에 따른 수정

--- src/test/java/strategy/EveryTwiceMovableStrategyTest.java
+++ src/test/java/strategy/EveryTwiceMovableStrategyTest.java
@@ -5,15 +5,14 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class EveryNTimesMovableStrategyTest {
+class EveryTwiceMovableStrategyTest {
     @DisplayName("정적팩터리 메서드를 이용하여 객체를 생성하면 객체가 생성된다.")
     @Test
     void createTest() {
         assertThat(EveryTwiceMovableStrategy.newInstance()).isInstanceOf(EveryTwiceMovableStrategy.class);
     }
 
-
-    @DisplayName("N번마다 움직일 수 있는 전략을 선택하면 움직임 여부는 N번마다 항상 참이다.")
+    @DisplayName("2번마다 움직일 수 있는 전략을 이용하여 움직임여부 호출횟수를 기준으로, 2번마다 항상 참이다.")
     @Test
     void isMovableTest() {
         MovableStrategy movableStrategy = EveryTwiceMovableStrategy.newInstance();
